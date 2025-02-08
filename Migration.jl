196cccd7660e9654ee0f4968aeb75fd0babb8fcf
Emilia Horgby <117984181+NilsHorgby@users.noreply.github.com>
2025-02-08 20:32:34 +0100

    Update Migration.jl
    An experiment with a different data structure

--- Migration.jl
+++ Migration.jl
@@ -1,18 +1,12 @@
 module Migration
-
-using .Main.Initialisation
+include("Initialisation.jl")
+using .Initialisation
+#using .Main.Initialisation
 using Distributions: Normal
 
 export migration
 
-struct SexSpecificPatch
-    individuals::Vector{Individual}
-    sex::Bool
-    location::Int
-end
-
-function put_within_boundaries(location::Int64,signed_distance::Int64, number_of_patches::Int64)
-    new_patch::Int64 = location + signed_distance
+function put_within_boundaries(new_patch::Int64, number_of_patches::Int64)
     if new_patch <= 0
         return 1
     elseif new_patch > number_of_patches
@@ -26,59 +20,58 @@ function discrete_distrubution(dispersal_distance::Float64)
     return Int(round(rand(Normal(0,dispersal_distance),1)[1]))
 end
 
-function migration(population::Vector{Patch},dispersal_distance::Float64, number_of_patches::Int64)::Vector{Patch}
-    #population = deepcopy(population) the population will be modified in place, but this doesn't matter
-    current_females = [SexSpecificPatch(patch.females,false, patch.location) for patch in population]
-    current_males = [SexSpecificPatch(patch.males,true, patch.location) for patch in population]
-    next_females::Vector{SexSpecificPatch} = [SexSpecificPatch([],false,population[i].location)
-                                              for i=eachindex(population)]
-    next_males::Vector{SexSpecificPatch} = [SexSpecificPatch([],true,population[i].location)
-                                            for i=eachindex(population)]
-    #migration one by one:
+
+array = bitrand((10,2))
+
+array[1,:] = [0,1]
+array[1,:]
+
+function migration(population::Vector{Patch},dispersal_distance::Float64)::Vector{Patch}
+    number_of_patches::Int64 = length(population)
+    number_individuals::Int64 = size(population[1].females)[1]
+    max_number_individuals_per_patch = round(Int,(1 + dispersal_distance) * 4 * size(population[1].females)[1])
+    current_females::Vector{BitMatrix} = [patch.females for patch in population]
+    current_males::Vector{BitMatrix} = [patch.males for patch in population]
+    next_females::Vector{BitMatrix} = [BitMatrix(undef,max_number_individuals_per_patch,2) for i=eachindex(population)]
+    next_males::Vector{BitMatrix} = [BitMatrix(undef,max_number_individuals_per_patch,2) for i=eachindex(population)]
     
-    function migrate_one_individual(population::Vector{SexSpecificPatch},next_population::Vector{SexSpecificPatch}, dispersal_distance::Float64)
-        individual::Individual= population[1].individuals[1]
-        signed_distance::Int64 = discrete_distrubution(dispersal_distance)
-        deleteat!(population[1].individuals,1)
-        
-        #Boundary condition: if the individual tries to go outside the area, it doesn't move
-        new_patch = put_within_boundaries(population[1].location,signed_distance, number_of_patches)
-        append!(next_population[new_patch].individuals,[individual])  
-        if length(population[1].individuals) == 0
-            deleteat!(population,1)
-        end
-        return population,next_population
-    end
     
-    #we should be able to do tail recursion here
-    function migrate(population::Vector{SexSpecificPatch},next_population::Vector{SexSpecificPatch}, dispersal_distance::Float64)
-        while population != []
-            population, next_population = migrate_one_individual(population,next_population,dispersal_distance)
+    function migrate(population::Vector{BitMatrix},next_population::Vector{BitMatrix}, dispersal_distance::Float64)
+        #signed_distances::Matrix{Int64} =  round.(Int,rand(Normal(0,dispersal_distance),(number_of_patches, number_of_in_patches)))
+        #next_locations::Matrix{Int64} = put_within_boundaries.(signed_distances .+ hcat([collect(1:number_of_patches) for i in 1:number_individuals]...),number_of_patches)
+        number_of_inds_in_patches = zeros(Int,number_of_patches)
+        @inbounds for location in 1:number_of_patches
+            for individual_index in 1:number_individuals
+                next_location::Int64 = put_within_boundaries(round(Int,rand(Normal(location,dispersal_distance))), number_of_patches)
+                number_of_inds_in_patches[next_location] += 1
+                next_index::Int64 = number_of_inds_in_patches[next_location]
+                next_population[next_location][next_index,:] = population[location][individual_index,:]
+            end
         end
-        return next_population
+        #((patch,number_of_inds_in_patch) -> patch[1:number_of_inds_in_patch,:]).(next_females,number_of_inds_in_patches )
+        return [patch[1:number_of_inds_in_patch,:] for (patch,number_of_inds_in_patch) in zip(next_females,number_of_inds_in_patches)]
     end
 
     
-    function combine_males_and_females(females::SexSpecificPatch,males::SexSpecificPatch)::Patch
-        @assert females.location == males.location "The locations are not the same"
-        return Patch(females.individuals,males.individuals, females.location)
-    end
-    population = combine_males_and_females.(migrate(current_females,next_females,dispersal_distance),migrate(current_males,next_males,dispersal_distance))
-    return population
+    return Patch.(migrate(current_females,next_females,dispersal_distance),migrate(current_males,next_males,dispersal_distance),1:number_of_patches)
 end
 
+using BenchmarkTools
+population = create_population(100,100)
+population[1].males
+migration(population,1.5)
+max(length.((x -> x.males).(migration(population,1.5)))...)
+migration(population,1.5)[1].males
 end
 """
-Benchmark for migration function
+BenchmarkTools.Trial: 6262 samples with 1 evaluation.
+Range (min … max):  541.119 μs …   7.071 ms  ┊ GC (min … max):  0.00% … 88.25%
+Time  (median):     648.125 μs               ┊ GC (median):     0.00%
+Time  (mean ± σ):   796.221 μs ± 750.201 μs  ┊ GC (mean ± σ):  18.90% ± 16.57%
 
-BenchmarkTools.Trial: 10000 samples with 1 evaluation.
- Range (min … max):  227.133 μs …  17.273 ms  ┊ GC (min … max):  0.00% … 90.34%
- Time  (median):     266.618 μs               ┊ GC (median):     0.00%
- Time  (mean ± σ):   387.354 μs ± 827.794 μs  ┊ GC (mean ± σ):  25.67% ± 11.62%
+▆█▆▃                                                        ▁ ▁
+████▇▅▁▃▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▃▆▇▇▇▇███ █
+541 μs        Histogram: log(frequency) by time       4.58 ms <
 
-  █▃▂▁                                                          ▁
-  ████▆▅▁▁▁▁▃▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▄▅▅▆▅▆▆▇ █
-  227 μs        Histogram: log(frequency) by time       5.91 ms <
-
- Memory estimate: 1.14 MiB, allocs estimate: 21494.
+Memory estimate: 1.91 MiB, allocs estimate: 60613.
 """
