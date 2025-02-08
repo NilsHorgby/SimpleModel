2b2423baa9fe3206e287fd9f1b010a5cbd51c31e
Emilia Horgby <117984181+NilsHorgby@users.noreply.github.com>
2025-02-08 15:40:32 +0100

    Update Migration.jl

--- Migration.jl
+++ Migration.jl
@@ -1,5 +1,5 @@
 module Migration
-include("Initialisation.jl")
+
 using .Main.Initialisation
 using Distributions: Normal
 
@@ -11,8 +11,22 @@ struct SexSpecificPatch
     location::Int
 end
 
+function put_within_boundaries(location::Int64,signed_distance::Int64, number_of_patches::Int64)
+    new_patch::Int64 = location + signed_distance
+    if new_patch <= 0
+        return 1
+    elseif new_patch > number_of_patches
+        return number_of_patches
+    else 
+        return new_patch
+    end
+end
 
-function migration(population::Vector{Patch})::Vector{Patch}
+function discrete_distrubution(dispersal_distance::Float64)
+    return Int(round(rand(Normal(0,dispersal_distance),1)[1]))
+end
+
+function migration(population::Vector{Patch},dispersal_distance::Float64, number_of_patches::Int64)::Vector{Patch}
     #population = deepcopy(population) the population will be modified in place, but this doesn't matter
     current_females = [SexSpecificPatch(patch.females,false, patch.location) for patch in population]
     current_males = [SexSpecificPatch(patch.males,true, patch.location) for patch in population]
@@ -21,25 +35,14 @@ function migration(population::Vector{Patch})::Vector{Patch}
     next_males::Vector{SexSpecificPatch} = [SexSpecificPatch([],true,population[i].location)
                                             for i=eachindex(population)]
     #migration one by one:
-
-    function put_within_boundaries(location,signed_distance)
-        new_patch::Int64 = location + signed_distance
-        if new_patch <= 0
-            return 1
-        elseif new_patch > number_of_patches
-            return number_of_patches
-        else 
-            return new_patch
-        end
-    end
     
-    function migrate_one_individual(population::Vector{SexSpecificPatch},next_population::Vector{SexSpecificPatch})
+    function migrate_one_individual(population::Vector{SexSpecificPatch},next_population::Vector{SexSpecificPatch}, dispersal_distance::Float64)
         individual::Individual= population[1].individuals[1]
-        signed_distance::Int64 = Int(round(rand(Normal(0,1.5),1)[1]))
+        signed_distance::Int64 = discrete_distrubution(dispersal_distance)
         deleteat!(population[1].individuals,1)
         
         #Boundary condition: if the individual tries to go outside the area, it doesn't move
-        new_patch = put_within_boundaries(population[1].location,signed_distance)
+        new_patch = put_within_boundaries(population[1].location,signed_distance, number_of_patches)
         append!(next_population[new_patch].individuals,[individual])  
         if length(population[1].individuals) == 0
             deleteat!(population,1)
@@ -48,9 +51,9 @@ function migration(population::Vector{Patch})::Vector{Patch}
     end
     
     #we should be able to do tail recursion here
-    function migrate(population::Vector{SexSpecificPatch},next_population::Vector{SexSpecificPatch})
+    function migrate(population::Vector{SexSpecificPatch},next_population::Vector{SexSpecificPatch}, dispersal_distance::Float64)
         while population != []
-            population, next_population = migrate_one_individual(population,next_population)
+            population, next_population = migrate_one_individual(population,next_population,dispersal_distance)
         end
         return next_population
     end
@@ -60,7 +63,7 @@ function migration(population::Vector{Patch})::Vector{Patch}
         @assert females.location == males.location "The locations are not the same"
         return Patch(females.individuals,males.individuals, females.location)
     end
-    population = combine_males_and_females.(migrate(current_females,next_females),migrate(current_males,next_males))
+    population = combine_males_and_females.(migrate(current_females,next_females,dispersal_distance),migrate(current_males,next_males,dispersal_distance))
     return population
 end
 
